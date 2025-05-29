c82af0ba63223d06eecbd9772db4c008ec06b12a
mostafaelhawary1987 <hoohoo1987@gmail.com>
2025-05-29 22:32:21 +0300

    index3

--- js/main.js
+++ js/main.js
@@ -54,6 +54,39 @@ $(window).scroll(function () {
     }
   }
 });
+        var owl = $("#banner-slider-demo-1").owlCarousel({
+        items: 1,
+        
+        autoplay: false,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true, 
+        dots: true,
+        dotsContainer: '#carousel-custom-dots',
+        nav: true,
+        navRewind: true,
+         navText: ["<span class='fa-regular fa-circle-arrow-right'></span>","<span class='fa-regular fa-circle-arrow-left'></span>"],
+        loop: false, });
+        $('.owl-dot').click(function(){owl.trigger('to.owl.carousel', [$(this).index(), 300]);});
+        //Random index generator
+        function randomPosition(){
+        var r_hb = $('#carousel-custom-dots li').length;
+        var randomize = null;
+        randomize=1
+        
+        
+        if (randomize != 0) {
+        return (Math.floor(Math.random()* r_hb));
+        }
+        else { return 0 ;}
+        }
+        //Sort random function
+        function random(owlSelector){
+        owlSelector.children().sort(function(){
+        return Math.round(Math.random()) - 0.5;
+        }).each(function(){
+        $(this).appendTo(owlSelector);
+        });
+        }
         ///////// landing header /////////
     $('a[data-scroll^="#"]').on('click', function (e) {
         e.preventDefault();
